c05ae31ace01fe4ffbe5b7f5bec993e4f904fc62
Vestyru <belozerskix010@gmail.com>
2025-03-23 01:00:17 +1000

    get
    get

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,1 @@
-/* const mainNumber = document.querySelector('.slider__main-number');
 
-let i = 4256;
-
-let time = setInterval(() => {
-    if (i === 5001) {
-        return;
-    }
-    mainNumber.textContent = i;
-    i++;
-}, 1); */
